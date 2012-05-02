ee52a5fe3b0a5d6e51585379a92c6e38698ece04
Tobias Crawley <tcrawley@gmail.com>
2012-05-01 23:25:51 -0400

    Extracted from lein-immutant - go there if you need more history.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject org.immutant/deploy-tools "0.6.0"
+  :description "Handy dandy tools for deploying and archiving Immutant applications."
+  :dependencies [[org.clojure/clojure "1.3.0"]])
