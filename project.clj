f2d38bbcfed2f772dd60fc8d1a93d0a4c4fed7ab
Tobias Crawley <tcrawley@gmail.com>
2012-06-12 10:49:53 -0400

    Back to snapshot.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject org.immutant/deploy-tools "0.7.0"
+(defproject org.immutant/deploy-tools "0.8.0-SNAPSHOT"
   :description "Handy dandy tools for deploying and archiving Immutant applications."
   :dependencies [[org.clojure/clojure "1.3.0"]])
