db854c405bfe1e079ca367993d65ab3b61971c22
Tobias Crawley <tcrawley@gmail.com>
2012-11-29 22:18:49 -0500

    Prep for release 0.10.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject org.immutant/deploy-tools "0.10.0-SNAPSHOT"
+(defproject org.immutant/deploy-tools "0.10.0"
   :description "Handy dandy tools for deploying and archiving Immutant applications."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :profiles {:dev {:resource-paths ["resources" "test-resources"]}})
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/immutant/deploy-tools"
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]
+                   :resource-paths ["resources" "test-resources"]}})
