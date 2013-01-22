b7f9931cfbe41fa1f01837e0ecec74ead1efef13
Toby Crawley <toby@tcrawley.org>
2013-01-22 15:51:01 -0500

    Back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/deploy-tools "0.11.1"
+(defproject org.immutant/deploy-tools "0.12.0-SNAPSHOT"
   :description "Handy dandy tools for deploying and archiving Immutant applications."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
