1fabf3c686a243ae3782ef475e839c6329cbe35d
Toby Crawley <toby@tcrawley.org>
2013-03-13 16:33:45 -0400

    Release 0.12.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.immutant/deploy-tools "0.12.0-SNAPSHOT"
+(defproject org.immutant/deploy-tools "0.12.0"
   :description "Handy dandy tools for deploying and archiving Immutant applications."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/immutant/deploy-tools"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]
-                   :resource-paths ["resources" "test-resources"]}})
+                   :resource-paths ["resources" "test-resources"]}}
+  :lein-release {:deploy-via :clojars})
