33742a94f9f4b789fa1b2a6b4984f7e7697c7749
Toby Crawley <toby@tcrawley.org>
2015-05-20 10:45:59 -0400

    Version 2.0.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/deploy-tools "2.0.5"
+(defproject org.immutant/deploy-tools "2.0.6-SNAPSHOT"
   :description "Tools for deploying Immutant applications to a container."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
