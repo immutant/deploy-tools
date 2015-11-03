703d2ab06ed4e1d8b9bf448fc649498cc7afb9dc
Toby Crawley <toby@tcrawley.org>
2015-11-03 10:08:29 -0500

    Update test to use incremental build and recognize new classpath structure.

--- test/immutant/deploy_tools/test/war.clj
+++ test/immutant/deploy_tools/test/war.clj
@@ -3,15 +3,22 @@
             [clojure.test :refer :all]
             [leiningen.core.classpath :as cp]))
 
+(defn classpath-chain [project]
+  (-> project insert-versions classpath))
+
 (deftest classpath-should-include-org-immutant-wildfly-for-the-given-immutant-version
-  (let [base-project {:dependencies [['org.immutant/fntest "2.0.3"]]
+  (let [immutant-version "2.x.incremental.669"
+        base-project {:dependencies [['org.immutant/fntest "2.0.3"]]
                       :dependency-resolver (partial cp/resolve-dependencies :dependencies)
                       :dependency-hierarcher (partial cp/dependency-hierarchy :dependencies)
                       :repositories [["central" {:snapshots false, :url "https://repo1.maven.org/maven2/"}]
-                                     ["clojars" {:url "https://clojars.org/repo/"}]]}]
+                                     ["clojars" {:url "https://clojars.org/repo/"}]
+                                     ["Immutant incremental builds"
+                                      {:url "http://downloads.immutant.org/incremental/"}]]}]
     (doseq [dep ["core" "immutant" "messaging" "scheduling" "transactions" "web"]]
-      (is (re-find #"org/immutant/wildfly/2.0.0-beta2"
-            (classpath
-              (update-in base-project [:dependencies] conj [(symbol "org.immutant" dep) "2.0.0-beta2"])))))
-    (is (thrown-with-msg? Exception #"No org.immutant dependency found"
-            (classpath base-project)))))
+      (is (some (partial re-find (re-pattern (str "org/immutant/wildfly/" immutant-version)))
+            (-> base-project
+              (update-in [:dependencies] conj [(symbol "org.immutant" dep) immutant-version])
+              classpath-chain))))
+    (is (thrown-with-msg? Exception #"No org.immutant/core dependency found"
+            (classpath-chain base-project)))))
